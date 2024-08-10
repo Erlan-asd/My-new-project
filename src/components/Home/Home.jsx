dddb83e69538783ea5db5f45c590707576be099f
Erlan-asd <Vipoffacc@gmail.com>
2024-08-10 12:20:23 +0600

    Last

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Categories from "../Categories/Categories";
 import Poster from "../Poster/Poster";
+import Products from "../Products/Products";
+import Banner from "../Banner/Banner";
+import { filterByPrice } from "../../features/products/productsSlice";
 
 const Home = () => {
+  const dispathch = useDispatch();
+  const {
+    products: { list, filtered },
+    categories,
+  } = useSelector((state) => state);
+
+  useEffect(() => {
+    if (!list.length) return;
+    dispathch(filterByPrice(100));
+  }, [dispathch, list.length]);
+
   return (
     <>
       <Poster />
+      <Products products={list} amount={5} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Worth seeing" />
+      <Banner />
+      <Products products={filtered} amount={5} title="Less than 100$" />
     </>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,38 +7,33 @@ const Products = ({ title, style = {}, products = [], amount }) => {
   const list = products.filter((_, i) => i < amount);
 
   return (
-    <section className={style.products} style={style}>
+    <section className={styles.products} style={style}>
       {title && <h2>{title}</h2>}
       <div className={styles.list}>
-        {products.map(
-          ({ id, images, title, category: { name: cat }, price }) => (
-            <Link to={`/products/${id}`} key={id} className={styles.product}>
-              <div
-                className={styles.image}
-                style={{ backgroundImage: `url(${images[0]})` }}
-              >
-                <div className={styles.wrapper}>
-                  <h3 className={styles.title}>{title}</h3>
-                  <div className={styles.cat}>
-                    <div className={styles.info}>
-                      <div className={styles.prices}>
-                        <div className={styles.price}>{price}$</div>
-                        <div className={styles.oldPrice}>
-                          {Math.floor(price * 0.8)}$
-                        </div>
-                      </div>
-                      <div className={styles.purchases}>
-                        {Math.floor(Math.random() * 20 + 1)} purchased
-                      </div>
-                    </div>
+        {list.map(({ id, images, title, category: { name: cat }, price }) => (
+          <Link to={`/products/${id}`} key={id} className={styles.product}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: `url(${images[0]})` }}
+            />
+            <div className={styles.wrapper}>
+              <h3 className={styles.title}>{title}</h3>
+              <div className={styles.cat}>{cat}</div>
+              <div className={styles.info}>
+                <div className={styles.prices}>
+                  <div className={styles.price}>{price}$</div>
+                  <div className={styles.oldPrice}>
+                    {Math.floor(price * 0.8)}$
                   </div>
                 </div>
+                <div className={styles.purchases}>
+                  {Math.floor(Math.random() * 20 + 1)} purchased
+                </div>
               </div>
-            </Link>
-          )
-        )}
+            </div>
+          </Link>
+        ))}
       </div>
-      ;
     </section>
   );
 };
